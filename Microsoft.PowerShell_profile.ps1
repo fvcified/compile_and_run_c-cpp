92e7f28e330b186b95eb992767bb6f1e2de42e05
13 <hhrizgrace@gmail.com>
2026-07-27 18:33:34 +0800

    Upd!

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+# --- 1. Compile Only ---
 function c {
     param([string]$file, [string]$out)
     if (-not $out) { $out = [System.IO.Path]::GetFileNameWithoutExtension($file) }
@@ -8,4 +9,19 @@ function cpp {
     param([string]$file, [string]$out)
     if (-not $out) { $out = [System.IO.Path]::GetFileNameWithoutExtension($file) }
     g++ $file -o $out
+}
+
+# --- 2. Compile & Run ---
+function cr {
+    param([string]$file)
+    $out = [System.IO.Path]::GetFileNameWithoutExtension($file)
+    gcc $file -o $out
+    if ($?) { .\"$out.exe" }
+}
+
+function cppr {
+    param([string]$file)
+    $out = [System.IO.Path]::GetFileNameWithoutExtension($file)
+    g++ $file -o $out
+    if ($?) { .\"$out.exe" }
 }
